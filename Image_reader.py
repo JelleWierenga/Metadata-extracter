bedd2a7bb6db566aeefd043f81bb28415c36d005
jelle <jellewierenga36@gmail.com>
2025-01-03 23:15:34 +0100

    remake for multiple file types (still testing some things)

--- Image_reader.py
+++ Image_reader.py
@@ -1,26 +1,39 @@
-import os
 from PIL import Image
 from PIL.ExifTags import TAGS
-from prettytable import PrettyTable
+import os
 
-table = PrettyTable()
+def get_file_type(extension):
+    file_types = {
+        "images": [".jpg", ".jpeg", ".png", ".tiff", ".bmp", ".gif"],
+        "videos": [".mp4", ".mov", ".avi", ".mkv"],
+        "ms_office": [".docx", ".xlsx", ".pptx"],
+        "pdfs": [".pdf"],
+        "text": [".txt"],
+    }
 
-table.field_names = ["MetaTags", "Values"]
+    for key, extensions in file_types.items():
+        if extension in extensions:
+            return key
+    return None
 
-folder = "./images"
-for image in os.listdir(folder):
-    path = os.path.join(folder, image)
+# folder = "./images"
+#
+# for image in os.listdir(folder):
+#     image = Image.open(f"images/{image}")
+#     exif_data = image.getexif()
+#     for tagid in exif_data:
+#         tagname = TAGS.get(tagid, tagid)
+#         value = exif_data.get(tagid)
+#         print(f"{tagname}: {value}")
+#     print("\n")
 
-    my_image = Image.open(path)
 
-    img_exif_data = my_image.getexif()
 
-    for id in img_exif_data:
-        tag_name = TAGS.get(id, id)
-        data = img_exif_data.get(id)
-        if isinstance(data, bytes):
-            data = data.decode()
+file_folder = "./files"
+for file in os.listdir(file_folder):
+    file_path = f"{file_folder}/{file}"
+    extension = os.path.splitext(file)[1].lower()
+    file_type = get_file_type(extension)
+    if file_type:
+        print(f"{file} is a {file_type} file with extension {extension}")
 
-        table.add_row([tag_name, data])
-
-    print(table)
